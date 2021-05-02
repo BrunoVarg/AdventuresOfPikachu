b35706752369b45e35cd4f5d2da1609247dc0793
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2021-05-02 18:31:22 -0300

    Update

--- AoL/main.asm
+++ AoL/main.asm
@@ -1,27 +1,27 @@
 .data
 .include "Capa.data"
-.include "pikachu_front.data"
+.include "/sprites/pikachu_front.data"
 .include "fase1.data"
 
 .text
 # Carrega a imagem1
 FORA:	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
 	li t2,0xFF012C00	# endereco final 
-	la s1,Capa		# endereço dos dados da tela na memoria
-	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
-LOOP1: 	beq t1,t2,KEYBOARD		# Se for o último endereço então sai do loop
+	la s1,Capa		# endereÃ§o dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informaÃ§Ãµes de nlin ncol
+LOOP1: 	beq t1,t2,KEYBOARD		# Se for o Ãºltimo endereÃ§o entÃ£o sai do loop
 	lw t3,0(s1)		# le um conjunto de 4 pixels : word
-	sw t3,0(t1)		# escreve a word na memória VGA
-	addi t1,t1,4		# soma 4 ao endereço
+	sw t3,0(t1)		# escreve a word na memÃ³ria VGA
+	addi t1,t1,4		# soma 4 ao endereÃ§o
 	addi s1,s1,4
 	j LOOP1			# volta a verificar
 
 KEYBOARD:
-	li t1,0xFF200000	# carrega o endereço de controle do KDMMIO
+	li t1,0xFF200000	# carrega o endereÃ§o de controle do KDMMIO
 	lw t0,0(t1)		# Le bit de Controle Teclado
 	andi t0,t0,0x0001	# mascara o bit menos significativo
-   	beq t0,zero,KEYBOARD   	# Se não há tecla pressionada então volta pro LOOP
-  	j PROX_LABEL  		# vai para a próxima LABEL se pressionar uma tecla
+   	beq t0,zero,KEYBOARD   	# Se nÃ£o hÃ¡ tecla pressionada entÃ£o volta pro LOOP
+  	j PROX_LABEL  		# vai para a prÃ³xima LABEL se pressionar uma tecla
   	
 PROX_LABEL:
 	li s0,0xFF200604	# Escolhe o Frame 0 ou 1
@@ -31,17 +31,17 @@ PROX_LABEL:
 	
 	li t1,0xFF100000	# endereco inicial da Memoria VGA - Frame 1
 	li t2,0xFF112C00	# endereco final 
-	la s1,fase1		# endereço dos dados da tela na memoria
-	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
-LOOP2: 	beq t1,t2,PRINT_IMAGE		# Se for o último endereço então sai do loop
+	la s1,fase1		# endereÃ§o dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informaÃ§Ãµes de nlin ncol
+LOOP2: 	beq t1,t2,PRINT_IMAGE		# Se for o Ãºltimo endereÃ§o entÃ£o sai do loop
 	lw t3,0(s1)		# le um conjunto de 4 pixels : word
-	sw t3,0(t1)		# escreve a word na memória VGA
-	addi t1,t1,4		# soma 4 ao endereço
+	sw t3,0(t1)		# escreve a word na memÃ³ria VGA
+	addi t1,t1,4		# soma 4 ao endereÃ§o
 	addi s1,s1,4
 	j LOOP2
 	
 PRINT_IMAGE:    
-    	# Endereço Inicial + Altura*320 + Largura = Printa imagem em qualquer lugar
+    	# EndereÃ§o Inicial + Altura*320 + Largura = Printa imagem em qualquer lugar
     
 	li s2, 160		# Largura = 160
 	li s1, 120		# Altura = 120
@@ -52,12 +52,12 @@ PRINT_IMAGE:
 	li a3,320
 	mul s3, s3, s1		# 320*(Altura) (Y)
 	li t1,0xFF100000	# endereco inicial da Memoria VGA - Frame 1
-	add t1, t1, s3		# Endereço Inicial + 320*(Altura) 
-	add t1, t1, s2		# Endereço Inicial + 320*(Altura) + (Largura)
+	add t1, t1, s3		# EndereÃ§o Inicial + 320*(Altura) 
+	add t1, t1, s2		# EndereÃ§o Inicial + 320*(Altura) + (Largura)
     
 	li t2,256		# endereco final
-	la s1,pikachu_front 	# endereço dos dados da tela na memoria
-	addi s1,s1,8        	# primeiro pixels depois das informações de nlin ncol
+	la s1,pikachu_front 	# endereÃ§o dos dados da tela na memoria
+	addi s1,s1,8        	# primeiro pixels depois das informaÃ§Ãµes de nlin ncol
     
 CICLO: 
 	bge t4,t2,FIM		
@@ -71,8 +71,8 @@ CICLO:
 	j CICLO   
          
 BREAK:
-	add t1,t1,a3		#Endereço +320 = Pula a linha
-	sub t1,t1,t0		#Endereço - 16 = Diminui pelo tamanho dos pixels
+	add t1,t1,a3		#EndereÃ§o +320 = Pula a linha
+	sub t1,t1,t0		#EndereÃ§o - 16 = Diminui pelo tamanho dos pixels
 	li t0,0
 	j CICLO			
 
