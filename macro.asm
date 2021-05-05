2280e7e7fc356091a2f619978ea44ac10074ea2b
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2021-05-05 18:59:32 -0300

    Procedimento_Print

--- macro.asm
+++ macro.asm
@@ -6,50 +6,6 @@
 #						#
 #################################################
 
-.macro print_image(%x,%y,%frame,%label)
-	li t3, 0
-	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
-	li t0,%frame
-	bgtz  t0, FRAME1_PI
-	j START1_PI
-FRAME1_PI:
-	li t0, 0x00100000
-	add t1,t1,t0		# vai pra Frame 1
-START1_PI:	
-	li s2, %x		# Largura = 160
-	li s1, %y		# Altura = 120
-	li t0,0
-	li s3,320		# multiplicar depois 
-	li a3,320
-	mul s3, s3, s1		# 320*(Altura) (Y)	
-	la a1,%label 		# endereço dos dados da tela na memoria		
-	lw s4, 0(a1)		# Largura da Imagem
-	lw s5, 4(a1)		# Altura da Imagem
-	mul t2, s4,s5
-	add t1, t1, s3		# Endereço Inicial + 320*(Altura) 
-	add t1, t1, s2		# Endereço Inicial + 320*(Altura) + (Largura)	
-	addi a1,a1,8        	# primeiro pixels depois das informações de nlin ncol
-    
-SAVE_PI: 
-	bge t4,t2,FINAL_PI		
-	addi t0,t0,4		
-	addi t4,t4,4		
-	lw t3,0(a1)         
-	sw t3,0(t1)         
-	addi t1,t1,4        
-	addi a1,a1,4	
-	beq t0,s4,BREAK1_PI
-	j SAVE_PI   
-         
-BREAK1_PI:
-	add t1,t1,a3		#Endereço + 320 = Pula a linha
-	sub t1,t1,t0		#Endereço - 16 = Diminui pelo tamanho dos pixels
-	li t0,0
-	j SAVE_PI
-FINAL_PI:
-	li t4, 0
-.end_macro
-
 .macro print_background(%label,%frame)
 	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
 	li t2,0xFF012C00	# endereco final - Frame 0
@@ -79,4 +35,11 @@ FIM_PB:
 	li s0,0xFF200604	# Escolhe o Frame 0 ou 1
 	li t2,%frame		# inicio Frame
 	sw t2,0(s0)
+.end_macro
+
+.macro load_values(%x,%y,%frame,%label)
+li t0, %frame
+li s2, %x
+li s1, %y
+la a1, %label
 .end_macro
--- main.asm
+++ main.asm
@@ -13,23 +13,33 @@ INICIO:
 	print_background(Capa,0)	# Printa a capa na frame 0
 	print_background(fase1,1)	# Printa a fase 1 na frame 1
 
-KEYBOARD:
-	li t1,0xFF200000	# carrega o endereço de controle do KDMMIO
-	lw t0,0(t1)		# Le bit de Controle Teclado
-	andi t0,t0,0x0001	# mascara o bit menos significativo
-   	beq t0,zero,KEYBOARD   	# Se não há tecla pressionada então volta pro LOOP
-  	j PROX_LABEL 		# vai para a próxima LABEL se pressionar uma tecla
+KEYBOARD_1:
+	# Descomentar no fim	
+	#li t1,0xFF200000	# carrega o endereço de controle do KDMMIO
+	#lw t0,0(t1)		# Le bit de Controle Teclado
+	#andi t0,t0,0x0001	# mascara o bit menos significativo
+   	#beq t0,zero,KEYBOARD   # Se não há tecla pressionada então volta pro LOOP
+  	#j PROX_LABEL 		# vai para a próxima LABEL se pressionar uma tecla
   	
 PROX_LABEL:
 	change_frame(1)
 	
-PRINT_IMAGE:	
-		
-	print_image(100,112,1,pikachu_back)
-	print_image(116,112,1,pikachu_front)
-	print_image(132,112,1,pikachu_right)
-	print_image(148,112,1,pikachu_left)
+j PRINT_1
+.include "print.asm"	
 	
+PRINT_1:
+	load_values(100,112,1,pikachu_front)
+	jal PRINT_IMAGE
+	load_values(116,112,1,pikachu_back)
+	jal PRINT_IMAGE
+	load_values(132,112,1,pikachu_right)
+	jal PRINT_IMAGE
+	load_values(148,112,1,pikachu_left)
+	jal PRINT_IMAGE
+
+KEYBOARD_LOOP:
+	
+
 
 
 # devolve o controle ao sistema operacional
