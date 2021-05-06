325551002d8f5f66e44d524d23db6fb68f2ebef5
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2021-05-06 20:23:45 -0300

    Pikachu_Movement

--- AdventuresOfLolo-main (1)/AdventuresOfLolo-main/main.asm
+++ AdventuresOfLolo-main (1)/AdventuresOfLolo-main/main.asm
@@ -19,11 +19,11 @@ INICIO:
 
 KEYBOARD_1:
 	# Descomentar no fim	
-	#li t1,0xFF200000	# carrega o endereÁo de controle do KDMMIO
+	#li t1,0xFF200000	# carrega o endere√ßo de controle do KDMMIO
 	#lw t0,0(t1)		# Le bit de Controle Teclado
 	#andi t0,t0,0x0001	# mascara o bit menos significativo
-   	#beq t0,zero,KEYBOARD   # Se n„o h· tecla pressionada ent„o volta pro LOOP
-  	#j PROX_LABEL 		# vai para a prÛxima LABEL se pressionar uma tecla
+   	#beq t0,zero,KEYBOARD   # Se n√£o h√° tecla pressionada ent√£o volta pro LOOP
+  	#j PROX_LABEL 		# vai para a pr√≥xima LABEL se pressionar uma tecla
   	
 PROX_LABEL:
 	change_frame(1)
