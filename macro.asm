e83b1ed5be551555306714fe92f26ba2d650eb42
Bruno Vargas <62037302+BrunoVarg@users.noreply.github.com>
2021-05-06 20:25:49 -0300

    macro Update

--- macro.asm
+++ macro.asm
@@ -15,16 +15,16 @@
 	j INICIO_PB
 SOMA_PB:
 	li t0, 0x00100000
-	add t1,t1,t0		# Endereço Inicial - Frame 1 - 0xFF100000
-	add t2,t2,t0		# Endereço Final - Frame 1 - 0xFF112C00
+	add t1,t1,t0		# EndereÃ§o Inicial - Frame 1 - 0xFF100000
+	add t2,t2,t0		# EndereÃ§o Final - Frame 1 - 0xFF112C00
 INICIO_PB:
 	 
-	la s1,%label		# endereço dos dados da tela na memoria
-	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
-LOOP1_PB: beq t1,t2,FIM_PB	# Se for o último endereço então sai do loop
+	la s1,%label		# endereÃ§o dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informaÃ§Ãµes de nlin ncol
+LOOP1_PB: beq t1,t2,FIM_PB	# Se for o Ãºltimo endereÃ§o entÃ£o sai do loop
 	lw t3,0(s1)		# le um conjunto de 4 pixels : word
-	sw t3,0(t1)		# escreve a word na memória VGA
-	addi t1,t1,4		# soma 4 ao endereço
+	sw t3,0(t1)		# escreve a word na memÃ³ria VGA
+	addi t1,t1,4		# soma 4 ao endereÃ§o
 	addi s1,s1,4
 	j LOOP1_PB		# volta a verificar
 FIM_PB:
@@ -42,4 +42,11 @@ li t0, %frame
 li s2, %x
 li s1, %y
 la a1, %label
+.end_macro
+
+# Verifica o caractere da KEYBOARD
+
+.macro verify(%char,%label)
+li t1, %char
+beq t1,t0,%label
 .end_macro
