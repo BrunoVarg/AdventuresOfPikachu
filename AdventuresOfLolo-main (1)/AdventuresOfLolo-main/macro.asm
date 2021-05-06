8a93b75b9016f5e2699eaa211f4a43da82a4c2e5
Ana Luísa <82478800+AnaluisaPA@users.noreply.github.com>
2021-05-06 20:22:04 -0300

    Add files via upload

---
+++ AdventuresOfLolo-main (1)/AdventuresOfLolo-main/macro.asm
@@ -0,0 +1,51 @@
+.data
+.text
+#################################################
+#						#
+#		PRINT				#
+#						#
+#################################################
+
+.macro print_background(%label,%frame)
+	li t1,0xFF000000	# endereco inicial da Memoria VGA - Frame 0
+	li t2,0xFF012C00	# endereco final - Frame 0
+	li t3,0
+	li t0,%frame
+	bgtz  t0, SOMA_PB
+	j INICIO_PB
+SOMA_PB:
+	li t0, 0x00100000
+	add t1,t1,t0		# Endereço Inicial - Frame 1 - 0xFF100000
+	add t2,t2,t0		# Endereço Final - Frame 1 - 0xFF112C00
+INICIO_PB:
+	 
+	la s1,%label		# endereço dos dados da tela na memoria
+	addi s1,s1,8		# primeiro pixels depois das informações de nlin ncol
+LOOP1_PB: beq t1,t2,FIM_PB	# Se for o último endereço então sai do loop
+	lw t3,0(s1)		# le um conjunto de 4 pixels : word
+	sw t3,0(t1)		# escreve a word na memória VGA
+	addi t1,t1,4		# soma 4 ao endereço
+	addi s1,s1,4
+	j LOOP1_PB		# volta a verificar
+FIM_PB:
+
+.end_macro
+
+.macro change_frame(%frame)
+	li s0,0xFF200604	# Escolhe o Frame 0 ou 1
+	li t2,%frame		# inicio Frame
+	sw t2,0(s0)
+.end_macro
+
+.macro load_values(%x,%y,%frame,%label)
+li t0, %frame
+li s2, %x
+li s1, %y
+la a1, %label
+.end_macro
+# Verifica o caractere da KEYBOARD
+
+.macro verify(%char,%label)
+li t1, %char
+beq t1,t0,%label
+.end_macro
---
+++ AdventuresOfLolo-main (1)/AdventuresOfLolo-main/main.asm
@@ -0,0 +1,138 @@
+.data
+
+POSITION: .half 100,112		# x e y
+
+.include "Capa.data"
+.include "/sprites/pikachu_front.data"
+.include "/sprites/pikachu_right.data"
+.include "/sprites/pikachu_left.data"
+.include "/sprites/pikachu_back.data"
+.include "fase1.data"
+
+
+.text
+.include "macro.asm"
+# Carrega a imagem1
+INICIO:
+	print_background(Capa,0)	# Printa a capa na frame 0
+	print_background(fase1,1)	# Printa a fase 1 na frame 1
+
+KEYBOARD_1:
+	# Descomentar no fim	
+	#li t1,0xFF200000	# carrega o endereço de controle do KDMMIO
+	#lw t0,0(t1)		# Le bit de Controle Teclado
+	#andi t0,t0,0x0001	# mascara o bit menos significativo
+   	#beq t0,zero,KEYBOARD   # Se não há tecla pressionada então volta pro LOOP
+  	#j PROX_LABEL 		# vai para a próxima LABEL se pressionar uma tecla
+  	
+PROX_LABEL:
+	change_frame(1)
+	
+j PRINT_1
+.include "print.asm"	
+	
+PRINT_1:
+	load_values(100,112,1,pikachu_front)
+	jal PRINT_IMAGE
+	load_values(116,112,1,pikachu_back)
+	jal PRINT_IMAGE
+	load_values(132,112,1,pikachu_right)
+	jal PRINT_IMAGE
+	load_values(148,112,1,pikachu_left)
+	jal PRINT_IMAGE
+
+KEYBOARD_LOOP:
+
+	li t1,0xFF200000
+	lw t0,0(t1)
+	andi t0,t0,0x0001
+  	beq t0,zero,KEYBOARD_LOOP
+  	lw t0,4(t1)			#Tecla pressionada = t0
+
+
+	verify('w',MOV_UP)
+	verify('s',MOV_DOWN)
+	verify('a',MOV_LEFT)
+	verify('d',MOV_RIGHT)
+	j KEYBOARD_LOOP
+
+MOV_UP:
+	
+	la a0, POSITION
+	lh t1, 0(a0)	# t1 = X
+	lh t2, 2(a0)	# t2 = Y
+	
+	addi s1,t2, -16
+	sh s1, 2(a0)
+	mv s2, t1
+	li t0,1
+	la a1, pikachu_back
+	jal PRINT_IMAGE
+	j KEYBOARD_LOOP
+	
+	#t0 = frame
+	#s2 = x
+	#s1 = y
+	#a1 = label
+
+MOV_DOWN:
+	
+	la a0, POSITION
+	lh t1, 0(a0)	# t1 = X
+	lh t2, 2(a0)	# t2 = Y
+	
+	addi s1,t2, 16
+	sh s1, 2(a0)
+	mv s2, t1
+	li t0,1
+	la a1, pikachu_front
+	jal PRINT_IMAGE
+	j KEYBOARD_LOOP
+	
+	#t0 = frame
+	#s2 = x
+	#s1 = y
+	#a1 = label
+
+MOV_LEFT:
+	
+	la a0, POSITION
+	lh t1, 0(a0)	# t1 = X
+	lh t2, 2(a0)	# t2 = Y
+	
+	addi s1,t1, -16
+	sh s1, 0(a0)
+	mv s2, t1
+	li t0,1
+	la a1, pikachu_left
+	jal PRINT_IMAGE
+	j KEYBOARD_LOOP
+	
+	#t0 = frame
+	#s2 = x
+	#s1 = y
+	#a1 = label
+
+MOV_RIGHT:
+	
+	la a0, POSITION
+	lh t1, 0(a0)	# t1 = X
+	lh t2, 2(a0)	# t2 = Y
+	
+	addi s1,t1, 16
+	sh s1, 0(a0)
+	mv s2, t1
+	li t0,1
+	la a1, pikachu_right
+	jal PRINT_IMAGE
+	j KEYBOARD_LOOP
+	
+	#t0 = frame
+	#s2 = x
+	#s1 = y
+	#a1 = label
+# devolve o controle ao sistema operacional
+FIM:
+	
+	li a7,10		# syscall de exit
+	ecall
